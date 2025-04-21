5a5c3b8c819a069fbb52a87a2c0827e6cd4233f9
raksha <raksha@192.168.1.8>
2025-04-21 19:00:29 +0545

    Added postgres database

---
+++ my_db.py
@@ -0,0 +1,28 @@
+from django import conf
+import psycopg2
+import os
+from dotenv import load_dotenv
+from calendar import c
+
+load_dotenv()
+
+try:
+    connection = psycopg2.connect(
+            dbname=os.getenv('DB_NAME'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT')
+        )
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT version();")
+
+    db_version = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    print("Database version:", db_version[0])
+
+except Exception as error:
+    print("Error while connecting to PostgreSQL:", error)
