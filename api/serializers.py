e2251918a4db4b849fc2ff242a5cd73b789ae879
raksha <karnraksha41@gmail.com>
2025-04-22 20:44:13 +0545

    Modified serializers

--- api/serializers.py
+++ api/serializers.py
@@ -2,17 +2,23 @@ from rest_framework import serializers
 from menu.models import MenuCategory, MenuItem, Restaurant
 
 
-class MenuCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MenuCategory
-        fields = '__all__'
-
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = '__all__'
 
+
+class MenuCategorySerializer(serializers.ModelSerializer):
+    items = MenuItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = MenuCategory
+        fields = '__all__'
+
+
 class RestaurantSerializer(serializers.ModelSerializer):
+    categories = MenuCategorySerializer(many=True, read_only=True)
+
     class Meta:
         model = Restaurant
         fields = '__all__'
