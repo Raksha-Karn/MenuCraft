72a6706b302504e7f7ba6f44a929074714d1bbd8
raksha <karnraksha41@gmail.com>
2025-04-22 18:22:54 +0545

    Registered on admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MenuItem, MenuCategory, Restaurant
 
 # Register your models here.
+admin.site.register(MenuItem)
+admin.site.register(MenuCategory)
+admin.site.register(Restaurant)
